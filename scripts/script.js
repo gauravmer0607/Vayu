c0f6b1d0e166f49b8d77b3d6dc57e112f8c56e34
gauravmer0607 <mergaurav4@gmail.com>
2025-08-24 21:16:27 +0530

    Resolved merge conflicts and updated files

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 function goAuth(){ 
   alert('Login/Signup page placeholder'); 
 }
@@ -8,15 +6,4 @@ function scrollToCards(){ document.getElementById('explore').scrollIntoView({
 }
 function goExplore(){
    window.location.href='explore.html'; 
-=======
-
-function goAuth(){ 
-  alert('Login/Signup page placeholder'); 
-}
-function scrollToCards(){ document.getElementById('explore').scrollIntoView({
-  behavior:'smooth'}); 
-}
-function goExplore(){
-   window.location.href='explore.html'; 
->>>>>>> 793bd99 (commit)
 }
